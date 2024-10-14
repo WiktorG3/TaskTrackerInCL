2fdf03efc850a86a524a525ba4c7b527bbd696a5
xXLordDedXx <wiktorek2003gro@wp.pl>
2024-10-14 18:55:35 +0200

    Added Basic Structure for Main Class

---
+++ TaskTracker.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class TaskTracker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean done = false;
+
+        while (!done) {
+            System.out.println("\nWelcome to Task Tracker!");
+            System.out.println("Choose what you would like to do:");
+            System.out.println("1. Add New Task");
+            System.out.println("2. List All Tasks");
+            System.out.println("3. Update Task");
+            System.out.println("4. Mark Task as In Progress");
+            System.out.println("5. Mark Task as Completed");
+            System.out.println("6. Delete Task");
+            System.out.println("0. Exit");
+            System.out.println("What's your choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addTask(scanner);
+                    break;
+                case 2:
+                    listTasks();
+                    break;
+                case 3:
+                    updateTask(scanner);
+                    break;
+                case 4:
+                    markTaskAsInProgress(scanner);
+                    break;
+                case 5:
+                    markTaskAsCompleted(scanner);
+                    break;
+                case 6:
+                    deleteTask(scanner);
+                    break;
+                case 0:
+                    System.out.println("See ya!");
+                    done = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice! Choose again!");
+            }
+        }
+        scanner.close();
+    }
+
+    private static void addTask(Scanner scanner) {
+        System.out.println("Enter Task Title: ");
+        String taskTitle = scanner.next();
+        System.out.println("Enter Task Description: ");
+        String taskDescription = scanner.next();
+        System.out.println("Enter Task Priority ( 1(highest) - 5(lowest)): ");
+        int taskPriority = scanner.nextInt();
+        System.out.println("Enter Task Status: ");
+        String taskStatus = scanner.next();
+        System.out.println("Enter Task Start Date: ");
+        String taskStartDate = scanner.next();
+
+        //Add Task
+    }
+
+    private static void listTasks() {
+        System.out.println("\nList of Tasks: ");
+        //Show Tasks
+
+    }
+
+    private static void updateTask(Scanner scanner) {
+        System.out.println("Enter Task Id to update: ");
+        int taskId = scanner.nextInt();
+    }
+
+    private static void markTaskAsInProgress(Scanner scanner) {
+        System.out.println("Enter Task Id to mark task as in progress: ");
+        int taskId = scanner.nextInt();
+    }
+
+    private static void markTaskAsCompleted(Scanner scanner) {
+        System.out.println("Enter Task Id to mark task as completed: ");
+        int taskId = scanner.nextInt();
+    }
+    private static void deleteTask(Scanner scanner) {
+        System.out.println("Enter Task Id to delete: ");
+        int taskId = scanner.nextInt();
+    }
+
+
+}
